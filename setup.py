fe41c1804c068c98433de4cd7f22fa31a568a8a1
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2021-04-12 03:47:12 +0500

    Screenshots updating

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('README.md') as f:
     long_description = f.read().strip()
 
 setup(name='decoder',
-	version='0.4',
+	version='0.5',
 	description='A simple script to try and decode a string in various encoding mechanisms regardless of it\'s (original) type.',
 	long_description=long_description,
 	long_description_content_type='text/markdown',  # This is important!
 	url='https://github.com/Anon-Exploiter/decoder',
 	author='Syed Umar Arfeen',
-	author_email='umar.arfeen@outlook.com',
+	author_email='18597330+Anon-Exploiter@users.noreply.github.com',
 	license='MIT',
 	packages=find_packages(),
 	install_requires=[
